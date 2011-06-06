643db3930aabcffa670b4d3489a32f24714e44ae
Roland Schütz <mail@rolandschuetz.at>
2011-06-06 20:45:19 +0200

    changed database.php for better defaults

--- app/Config/database.php
+++ app/Config/database.php
@@ -62,9 +62,9 @@ class DATABASE_CONFIG {
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
 		'host' => 'localhost',
-		'login' => 'user',
-		'password' => 'password',
-		'database' => 'database_name',
+		'login' => 'bancha',
+		'password' => 'bancha',
+		'database' => 'bancha',
 		'prefix' => '',
 	);
 
@@ -72,9 +72,9 @@ class DATABASE_CONFIG {
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
 		'host' => 'localhost',
-		'login' => 'user',
-		'password' => 'password',
-		'database' => 'test_database_name',
+		'login' => 'bancha',
+		'password' => 'bancha',
+		'database' => 'bancha',
 		'prefix' => '',
 	);
 }
