6c47f5cf1d6987f1324768f0ed9c984e4bb14ceb
Roland Schütz <mail@rolandschuetz.at>
2011-08-31 16:31:58 +0200

    Added js-extension to the router

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -30,3 +30,9 @@
  * ...and connect the rest of 'Pages' controller's urls.
  */
 	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
+
+
+/*
+ * Bancha needs to be able to deliver javascript files
+ */
+    Router::parseExtensions('js');
