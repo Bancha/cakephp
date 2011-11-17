b4e86725ea914429594ec56a211031cd6498f55e
Roland Schuetz <mail@rolandschuetz.at>
2011-11-17 23:31:23 +0100

    Now also displays serverside errors in the samples

--- app/webroot/js/error-handling.js
+++ app/webroot/js/error-handling.js
@@ -5,4 +5,8 @@ Ext.Error.handle = function(err) {
     Ext.Msg.alert('Error', err.msg);
 };
 
+Ext.direct.Manager.on('exception', function(err){
+	Ext.Msg.alert('Server-Error:'+err.message,"Where:<br />"+err.where+"<br /><br />trace:<br />"+err.trace);
+});
+
 // eof
